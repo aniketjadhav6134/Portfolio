8828e0f0db43cf8df7a43f3c6732e38293ac6090
aniketjadhav6134 <164015482+aniketjadhav6134@users.noreply.github.com>
2024-05-15 11:22:21 +0530

    first commit
    portfolio

---
+++ script.js
@@ -0,0 +1,41 @@
+var typed = new Typed('#typing-1', {
+    strings: ['Student', 'Coder', 'Developer'],
+    typeSpeed: 40,
+    backspeed: 40,
+    loop: true,
+});
+var sidemenu = document.getElementById('sidemenu');
+var menubtns = document.getElementsByClassName("menu-btn");
+const addclass = () => {
+    for (const menubtn of menubtns) {
+        menubtn.classList.remove("active");
+    }
+
+    event.currentTarget.classList.add("active");
+    sidemenu.style.left = "-100vw";
+    document.getElementById('none').style.left = "90vw";
+}
+
+var tablinks = document.getElementsByClassName("tablinks");
+var tabcontents = document.getElementsByClassName("tab-content");
+const opentab = (tabname) => {
+    for (const tablink of tablinks) {
+        tablink.classList.remove("active-link");
+    }
+    for (const tabcontent of tabcontents) {
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+const openmenu = () => {
+    sidemenu.style.left = "0vw";
+    document.getElementById('none').style.left = "-10vw";
+}
+
+const closemenu = () => {
+    sidemenu.style.left = "-100vw"
+    document.getElementById('none').style.left = "90vw";
+}
+
